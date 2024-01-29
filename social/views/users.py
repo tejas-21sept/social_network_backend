eb7899c51329cc4f859670cd3ba5aca0d82162cc
Tejas2109 <105809346+tejas-21sept@users.noreply.github.com>
2024-01-29 08:04:58 +0530

    added docstring

--- social/views/users.py
+++ social/views/users.py
@@ -71,158 +71,34 @@ class UserSearchAPIView(APIView):
             )
 
 
-# class FriendRequestAPIView(APIView):
-#     """
-#     API view for handling friend requests.
-
-#     Endpoint: /api/users/?search=tejas&page=1
-#     Methods:
-#     - POST: Send a friend request.
-#     - PUT: Accept or reject a friend request.
-
-#     POST Parameters:
-#     - from_user: User ID sending the friend request.
-#     - to_user: User ID receiving the friend request.
-
-#     PUT Parameters:
-#     - action: Action to perform ('accept' or 'reject').
-
-#     Returns:
-#     - If successful, returns the friend request details.
-#     - If unsuccessful, returns an error response with details.
-
-#     Status Codes:
-#     - 201 Created: Friend request sent successfully.
-#     - 200 OK: Friend request accepted or rejected successfully.
-#     - 400 Bad Request: Invalid input data.
-#     - 404 Not Found: Friend request not found.
-#     - 500 Internal Server Error: Server error during processing.
-#     """
-
-#     permission_classes = [IsAuthenticated]
-#     authentication_classes = [JWTAuthentication]
-
-#     def post(self, request):
-#         """
-#         Send a friend request.
-
-#         :param request: Django REST Framework request object.
-#         :return: Response with friend request details or error details.
-#         """
-#         try:
-#             if to_user_id := request.data.get("to_user", None):
-#                 return self._extracted_from_post_14(to_user_id, request)
-#             return Response(
-#                 api_response(
-#                     status_code=400,
-#                     message={"detail": "to_user is not provided."},
-#                 ),
-#                 status=status.HTTP_400_BAD_REQUEST,
-#             )
-#         except User.DoesNotExist:
-#             return Response(
-#                 api_response(
-#                     status_code=400,
-#                     message={"detail": "User does not exist."},
-#                 ),
-#                 status=status.HTTP_400_BAD_REQUEST,
-#             )
-
-#         except Exception as e:
-#             return Response(
-#                 api_response(status_code=500, message=str(e)),
-#                 status=status.HTTP_500_INTERNAL_SERVER_ERROR,
-#             )
-
-#     def _extracted_from_post_14(self, to_user_id, request):
-#         to_user = User.objects.filter(pk=to_user_id).first()
-#         # Check if the authenticated user matches the 'from_user'
-#         if request.user == to_user:
-#             return Response(
-#                 api_response(
-#                     status_code=401,
-#                     message={
-#                         "detail": "You can only send friend requests on your behalf."
-#                     },
-#                 ),
-#                 status=status.HTTP_401_UNAUTHORIZED,
-#             )
-
-#         if existing_request := FriendRequest.objects.filter(
-#             from_user=request.user, to_user=to_user
-#         ).first():
-#             return Response(
-#                 api_response(
-#                     status_code=400,
-#                     message={"detail": "Friend request already exists."},
-#                 ),
-#                 status=status.HTTP_400_BAD_REQUEST,
-#             )
-
-#         friend_request = FriendRequest(
-#             from_user=request.user, to_user=to_user, status="pending"
-#         )
-#         friend_request.save()
-
-#         serializer = FriendRequestSerializer(friend_request)
-#         return Response(
-#             api_response(status_code=201, data=serializer.data),
-#             status=status.HTTP_201_CREATED,
-#         )
-
-#     def patch(self, request, *args, **kwargs):
-#         try:
-#             print(f"\nargs - {args} - {kwargs}\n")
-#             pk = self.kwargs.get("pk", None)
-#             print(f"\npk - {pk}\n")
-#             friend_request = FriendRequest.objects.filter(pk=pk).first()
-#             print(f"\nfriend_request - {friend_request}\n")
-#             action = request.data.get("action", "reject")
-#             print(f"\naction - {action}\n")
-
-#             if action == "accept":
-#                 friend_request.status = "accepted"
-#             elif action == "reject":
-#                 friend_request.status = "rejected"
-#             else:
-#                 return Response(
-#                     api_response(
-#                         status_code=400,
-#                         message={"detail": "Invalid action."},
-#                     ),
-#                     status=status.HTTP_400_BAD_REQUEST,
-#                 )
-
-#             friend_request.save()
-#             serializer = FriendRequestSerializer(friend_request)
-#             return Response(
-#                 api_response(status_code=200, message=str(e)),
-#                 status=status.HTTP_200_OK,
-#             )
-
-#         except FriendRequest.DoesNotExist:
-#             return Response(
-#                 api_response(status_code=404, message=serializer.data),
-#                 status=status.HTTP_404_NOT_FOUND,
-#             )
-#         except Exception as e:
-#             print(e)
-#             return Response(
-#                 api_response(
-#                     status_code=500,
-#                     message=str(e),
-#                 ),
-#                 status=status.HTTP_500_INTERNAL_SERVER_ERROR,
-#             )
-
-
 class FriendRequestViewSet(viewsets.GenericViewSet):
+    """
+    A ViewSet for handling FriendRequest operations.
+
+    Attributes:
+    - permission_classes: List of permission classes to apply for authentication.
+    - authentication_classes: List of authentication classes to apply for user authentication.
+    - queryset: Queryset representing all FriendRequest objects.
+    - serializer_class: Serializer class to use for FriendRequest serialization.
+    """
+
     permission_classes = [IsAuthenticated]
     authentication_classes = [JWTAuthentication]
     queryset = FriendRequest.objects.all()
     serializer_class = FriendRequestSerializer
 
     def create(self, request, *args, **kwargs):
+        """
+        Create a new FriendRequest.
+
+        Parameters:
+        - request: The request object containing data for creating the FriendRequest.
+        - args: Additional arguments.
+        - kwargs: Additional keyword arguments.
+
+        Returns:
+        - Response: HTTP response indicating the success or failure of the request.
+        """
         try:
             if to_user_id := request.data.get("to_user", None):
                 return self._extracted_from_post_14(to_user_id, request)
@@ -241,7 +117,6 @@ class FriendRequestViewSet(viewsets.GenericViewSet):
                 ),
                 status=status.HTTP_400_BAD_REQUEST,
             )
-
         except Exception as e:
             return Response(
                 api_response(status_code=500, message=str(e)),
@@ -249,6 +124,16 @@ class FriendRequestViewSet(viewsets.GenericViewSet):
             )
 
     def _extracted_from_post_14(self, to_user_id, request):
+        """
+        Extract and process the creation of a FriendRequest.
+
+        Parameters:
+        - to_user_id: ID of the user to whom the friend request is sent.
+        - request: The request object.
+
+        Returns:
+        - Response: HTTP response indicating the success or failure of the request.
+        """
         to_user = User.objects.filter(pk=to_user_id).first()
         # Check if the authenticated user matches the 'from_user'
         if request.user == to_user:
@@ -285,6 +170,17 @@ class FriendRequestViewSet(viewsets.GenericViewSet):
         )
 
     def update(self, request, *args, **kwargs):
+        """
+        Update the status of a FriendRequest.
+
+        Parameters:
+        - request: The request object containing data for updating the FriendRequest status.
+        - args: Additional arguments.
+        - kwargs: Additional keyword arguments.
+
+        Returns:
+        - Response: HTTP response indicating the success or failure of the request.
+        """
         try:
             friend_request = get_object_or_404(self.get_queryset(), pk=kwargs.get("pk"))
             friend_request = self.get_object()
