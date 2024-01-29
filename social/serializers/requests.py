486c52b8e23201af023854e9a5e1df30199a669d
Tejas2109 <105809346+tejas-21sept@users.noreply.github.com>
2024-01-29 08:49:11 +0530

    created api for pensing friend request

--- social/serializers/requests.py
+++ social/serializers/requests.py
@@ -1,19 +1,42 @@
 from rest_framework import serializers
 
 from social.models import FriendRequest
+from social.serializers.user import UserSerializer
 
 
+# social/serializers.py
 class FriendRequestSerializer(serializers.ModelSerializer):
     """
-    Serializer for FriendRequest model.
+    Serializer class for FriendRequest model instances.
 
-    Fields:
-    - from_user: User ID sending the friend request.
-    - to_user: User ID receiving the friend request.
-    - status: Status of the friend request (choices: 'pending', 'accepted', 'rejected').
-    - created_at: Timestamp when the friend request was created (auto-generated).
+    Attributes:
+    - from_user_data: Serialized representation of the 'from_user' field using UserSerializer.
+    - to_user_data: Serialized representation of the 'to_user' field using UserSerializer.
+
+    Meta:
+    - model: The model class associated with this serializer.
+    - fields: The fields to be included in the serialized output.
     """
 
+    from_user_data = UserSerializer(source="from_user", read_only=True)
+    to_user_data = UserSerializer(source="to_user", read_only=True)
+
     class Meta:
+        """
+        Meta class for FriendRequestSerializer.
+
+        Attributes:
+        - model: The model class associated with this serializer.
+        - fields: The fields to be included in the serialized output.
+        """
+
         model = FriendRequest
-        fields = "__all__"
+        fields = [
+            "id",
+            "status",
+            "created_at",
+            "from_user",
+            "from_user_data",
+            "to_user",
+            "to_user_data",
+        ]
